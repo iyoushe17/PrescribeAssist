5e3477fdadc3dd32bdcfcffa649c4bd138d45f8e
Aayushi <aayushisingh3071@gmail.com>
2021-06-19 21:12:58 +0530

    bug fix

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template("index.html")
+    return render_template("home.html")
 
 @app.route('/predict', methods=['POST'])
 def predict():
